1ea7f4bfcbb25a46b1a8b29f957820e9e162cd37
moinuddin <moinminhal893@gmail.com>
2025-12-10 16:18:29 +0500

    Fix sidebar for Vercel & GitHub Pages deployment

--- sidebars.js
+++ sidebars.js
@@ -1,52 +1,18 @@
-// @ts-check
-
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  tutorialSidebar: [
+  docs: [
     {
       type: 'category',
-      label: 'Introduction',
-      items: ['intro'],
-      link: {
-        type: 'doc',
-        id: 'intro',
-      },
-    },
-    {
-      type: 'category',
-      label: 'Module 1: The Robotic Nervous System (ROS 2)',
+      label: 'Physical AI & Humanoid Robotics Course',
       items: [
         'module1/robotic-nervous-system-ros2',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Module 2: The Digital Twin (Gazebo & Unity)',
-      items: [
         'module2/digital-twin-gazebo-unity',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Module 3: The AI-Robot Brain (NVIDIA Isaac™)',
-      items: [
         'module3/ai-robot-brain-nvidia-isaac',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Module 4: Vision-Language-Action (VLA) Systems',
-      items: [
         'module4/vision-language-action-vla',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'RAG Chat Interface',
-      items: [
         'rag-interface',
       ],
-    }
+      collapsed: false,
+    },
   ],
 };
 
