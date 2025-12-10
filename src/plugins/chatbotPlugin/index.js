f7a336bb993b775a52db07753716ebb02f6f8e8a
moinuddin <moinminhal893@gmail.com>
2025-12-10 18:29:52 +0500

    FINAL FIX: Correct RagChatbot plugin path – deploy ready

--- src/plugins/chatbotPlugin/index.js
+++ src/plugins/chatbotPlugin/index.js
@@ -1,23 +1,26 @@
 // @ts-check
+
 /** @type {import('@docusaurus/types').PluginModule} */
-module.exports = function chatbotPlugin(context, options) {
+const chatbotPlugin = (context, options) => {
   return {
     name: 'chatbot-plugin',
-    
+
     getClientModules() {
       return [
-        require.resolve('./src/components/RagChatbot.js'),
-        require.resolve('./src/components/ContentButtons.js')
+        require.resolve('./../../components/RagChatbot'),
+        require.resolve('./../../components/ContentButtons')
       ];
     },
-    
+
     // Add the chatbot to all pages
     injectHtmlTags() {
       return {
         postBodyTags: [
-          `<div id="chatbot-container"></div>`,
+          '<div id="chatbot-container"></div>',
         ],
       };
     },
   };
 };
+
+module.exports = chatbotPlugin; 
