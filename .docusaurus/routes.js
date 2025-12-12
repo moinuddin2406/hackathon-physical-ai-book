93a698cdfece2ca773183202f68dd9d396ad1bf0
moinuddin <moinminhal893@gmail.com>
2025-12-12 14:27:43 +0500

    Add GitHub Actions deploy workflow

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,6 +3,41 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug', 'ac7'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/config',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/config', 'b61'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/content',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/content', '743'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/globalData',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/globalData', '4f7'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/metadata',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/metadata', '5ec'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/registry',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/registry', 'fb3'),
+    exact: true
+  },
+  {
+    path: '/hackathon-physical-ai-book/__docusaurus/debug/routes',
+    component: ComponentCreator('/hackathon-physical-ai-book/__docusaurus/debug/routes', 'e7f'),
+    exact: true
+  },
+  {
     path: '/hackathon-physical-ai-book/auth/background',
     component: ComponentCreator('/hackathon-physical-ai-book/auth/background', 'd24'),
     exact: true
